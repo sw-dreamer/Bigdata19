d804e5ffc8baea9f9903218b7a3faf010fc6fea8
sw <sw.woo.dreamer@gmail.com>
2025-01-24 10:26:46 +0900

    Update contactMain.java

--- 03. 연락처 관리 프로그램 (Hashmap 사용)/main/contactMain.java
+++ 03. 연락처 관리 프로그램 (Hashmap 사용)/main/contactMain.java
@@ -1,21 +1,9 @@
 package contactMVC.main;
 
-import java.util.Scanner;
-
 import contactMVC.control.serviceControl;
 
 public class contactMain {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("HashMap 또는 DB로 입력해주세요 > ");
-		String what = scanner.nextLine();
-		if (what.toLowerCase().equals("hashmap")) {
-			serviceControl.contactControl("hashmap");
-		} else if (what.toLowerCase().equals("db")) {
-			serviceControl.contactControl("DB");
-		} else {
-			System.out.println("잘못 입력하였습니다.");
-		}
-		scanner.close();
+		serviceControl.contactControl("hashmap");
 	}
 }
